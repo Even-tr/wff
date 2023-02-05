ce8b87665c09c05d8cb1a1eb49b3e57fef5536fa
Even-tr <tronstad.even@gmail.com>
2023-02-06 00:29:37 +0100

    Fixed circular import error

--- graphing.py
+++ graphing.py
@@ -1,5 +1,6 @@
-from script import wff, wff_node
+#from script import wff, wff_node
 import graphviz
+
 
 def simple_graphviz_test():
     dot = graphviz.Digraph(comment='The Round Table')
@@ -12,7 +13,7 @@ def simple_graphviz_test():
     dot.edge('B', 'L', constraint='false')
     dot.render('doctest-output/round-table.gv', view=True) 
 
-def make_DOT_tree(formula: wff_node, graph: graphviz.Digraph=None, parent: wff_node = None):
+def make_DOT_tree(formula: 'wff_node', graph: graphviz.Digraph=None, parent: 'wff_node' = None):
     assert type(graph) == graphviz.Graph, f"graph sould be of type 'graphviz.Graph', not '{str(type(graph))}'"
     graph.node(str(id(formula)), formula.symbol)
     if parent:
